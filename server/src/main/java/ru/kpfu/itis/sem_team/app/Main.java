df00e26e1b6ffc4b609f2684d7ff6c019f6e1265
atheostheos <coal.marcos@gmail.com>
2021-12-23 14:13:22 +0300

    ST-16 Make multithreaded server

--- server/src/main/java/ru/kpfu/itis/sem_team/app/Main.java
+++ server/src/main/java/ru/kpfu/itis/sem_team/app/Main.java
@@ -1,11 +1,12 @@
 package ru.kpfu.itis.sem_team.app;
 
+import ru.kpfu.itis.sem_team.protocol.UnoProtocol;
 import ru.kpfu.itis.sem_team.server.IServer;
 import ru.kpfu.itis.sem_team.server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        IServer server = new Server();
+        IServer server = new Server(UnoProtocol.STANDARD_PORT);
         server.init();
 
         server.listen();
--- server/src/main/java/ru/kpfu/itis/sem_team/server/Server.java
+++ server/src/main/java/ru/kpfu/itis/sem_team/server/Server.java
@@ -4,103 +4,143 @@ import ru.kpfu.itis.sem_team.exceptions.ServerException;
 import ru.kpfu.itis.sem_team.listeners.IServerEventListener;
 import ru.kpfu.itis.sem_team.message.IMessage;
 
-import java.io.Closeable;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Server implements IServer, Closeable {
-    public static final int DEFAULT_PORT = 27027;
-    private final List<IServerEventListener> eventListeners;
-    private ServerSocket server;
-    private final List<Socket> sockets;
+public class Server implements IServer {
+    private ServerSocketChannel server;
+    private Selector selector;
+    private final List<IServerEventListener> listeners;
     private final int port;
+    private final List<SocketChannel> channels;
 
     public Server(int port) {
+        this.listeners = new ArrayList<>();
+        this.channels = new ArrayList<>();
         this.port = port;
-        this.eventListeners = new ArrayList<>();
-        this.sockets = new ArrayList<>();
-    }
-
-    public Server() {
-        this(DEFAULT_PORT);
     }
 
     @Override
     public void init() {
         try {
-            server = new ServerSocket(port);
+            server = ServerSocketChannel.open();
+            InetSocketAddress addr = new InetSocketAddress(this.port);
+
+            server.bind(addr);
+            server.configureBlocking(false);
+
+            selector = Selector.open();
+            server.register(selector, SelectionKey.OP_ACCEPT);
+
             System.out.println("Server started");
         } catch (IOException e) {
-            throw new ServerException("Unable to start server");
+            throw new ServerException("Unable to start server", e);
         }
     }
 
     @Override
     public void listen() {
-        if (server == null) init();
-
-        while (true) {
+        while (selector.isOpen()) {
             try {
-                Socket socket = server.accept();
-                System.out.println("New connection: " + socket.toString());
-                handleConnection(socket);
+                int num = selector.select();
+                if (num == 0) continue;
+                Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+                while(it.hasNext()) {
+                    SelectionKey key = it.next();
+                    it.remove();
+                    processKey(key);
+                }
             } catch (IOException e) {
-                throw new ServerException("Error occurred while waiting for new connections");
+                throw new ServerException("Something happened while listening for new messages", e);
             }
         }
     }
 
-    private void handleConnection(Socket socket) {
-        try {
-            sockets.add(socket);
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-
-            IMessage message = (IMessage) ois.readObject();
-
-            int socketId = sockets.lastIndexOf(socket);
-
-            eventListeners.forEach(eventListener -> eventListener.handle(socketId, message));
-
-        } catch (ClassNotFoundException | IOException e) {
-            throw new ServerException("Unable to get message", e);
+    private void processKey(SelectionKey key) {
+        if (key.isAcceptable()) {
+            acceptConnection();
+        }
+        else if (key.isReadable()) {
+            SocketChannel channel = (SocketChannel) key.channel();
+            readMessage(channel);
         }
     }
 
-    /**
-     * Adds event listener to server, additionally connecting listener to itself,
-     * thus connecting it manually is unnecessary.
-     */
+    private void acceptConnection() {
+        try {
+            SocketChannel channel = server.accept();
+            System.out.println("New connection "+ channel);
+            channels.add(channel);
+            channel.configureBlocking(false);
+            channel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        } catch (IOException e) {
+            throw new ServerException("Unable to accept connection", e);
+        }
+    }
+
+    private void readMessage(SocketChannel channel) {
+        try {
+            int connectionId = channels.indexOf(channel);
+            //get message size
+            ByteBuffer messageSizeBuffer = ByteBuffer.allocate(4);
+            channel.read(messageSizeBuffer);
+            messageSizeBuffer.rewind();
+            int messageSize = messageSizeBuffer.getInt();
+
+            if (messageSize == 0) {
+                closeConnection(connectionId);
+                return;
+            }
+
+            //deserialize message
+            ByteBuffer messageBuffer = ByteBuffer.allocate(messageSize);
+            channel.read(messageBuffer);
+            messageBuffer.rewind();
+            ObjectInputStream ois = new ObjectInputStream( new ByteArrayInputStream(messageBuffer.array()));
+            IMessage message = (IMessage) ois.readObject();
+
+            //pass message to listeners
+            listeners.forEach(listener -> listener.handle(connectionId, message));
+
+        } catch (ClassNotFoundException | IOException e) {
+            throw new ServerException("Unable to read message", e);
+        }
+    }
+
+    private void closeConnection(int connectionId) {
+        try {
+            SocketChannel channel = channels.get(connectionId);
+            channel.close();
+            System.out.println("Connection " + connectionId + " closed");
+        } catch (IOException e) {
+            throw new ServerException("Unable to close connection", e);
+        }
+    }
+
     @Override
     public void addEventListener(IServerEventListener eventListener) {
         eventListener.init(this);
-        eventListeners.add(eventListener);
+        listeners.add(eventListener);
     }
 
     @Override
     public void sendMessage(int connectionId, IMessage message) {
         try {
-            Socket socket = sockets.get(connectionId);
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            oos.writeObject(message);
-            oos.flush();
+            SocketChannel channel = channels.get(connectionId);
+            ByteBuffer messageBuffer = ByteBuffer.wrap(message.formMessage());
+            channel.write(messageBuffer);
         } catch (IOException e) {
             throw new ServerException("Unable to send message", e);
         }
-    }
-
-    @Override
-    public void close() throws IOException {
-        if (server != null && !server.isClosed()) {
-            server.close();
-        }
-    }
-
-    public int getPort() {
-        return port;
     }
 }

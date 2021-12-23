978fda121903d248dae846cd54527d0ab2abf6bf
atheostheos <coal.marcos@gmail.com>
2021-12-23 14:21:12 +0300

    Update protocol

--- protocol/src/main/java/ru/kpfu/itis/sem_team/message/IMessage.java
+++ protocol/src/main/java/ru/kpfu/itis/sem_team/message/IMessage.java
@@ -6,5 +6,10 @@ import java.util.Map;
 public interface IMessage {
     Map<String, Object> getParameters();
     Object getParameter(String name);
+    <T> T getParameter(Class<T> c);
     void addParameter(String name, Object value);
+    /**
+     * Forms message into byte array suitable for sending by server/client
+     */
+    byte[] formMessage();
 }

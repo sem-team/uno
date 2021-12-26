af7abaa26d2bf8eb6d729bacc0eeea123d953929
ilnas <genatula@protonmail.com>
2021-12-26 20:53:12 +0300

    Write get card method

--- client/src/main/java/ru/kpfu/itis/sem_team/entities/boards/UnoBoard.java
+++ client/src/main/java/ru/kpfu/itis/sem_team/entities/boards/UnoBoard.java
@@ -1,16 +1,14 @@
 package main.java.ru.kpfu.itis.sem_team.entities.boards;
 
-import main.java.ru.kpfu.itis.sem_team.entities.cards.Color;
-import main.java.ru.kpfu.itis.sem_team.entities.cards.UnoCard;
+import main.java.ru.kpfu.itis.sem_team.entities.cards.*;
 import main.java.ru.kpfu.itis.sem_team.entities.games.AbstractGame;
 
-import java.util.Deque;
+import java.util.Random;
 
 public class UnoBoard extends AbstractBoard {
 
     private Color color;
     private boolean clockwise;
-//    private Deque<UnoCard> stackOfCard;
     private UnoCard lastUsedCard;
 
     public UnoBoard(AbstractGame game) {
@@ -18,7 +16,26 @@ public class UnoBoard extends AbstractBoard {
     }
 
     public UnoCard getCard() {
-
+        Random random = new Random();
+        double randomDouble = random.nextDouble();
+        if (randomDouble <= 0.71) {
+            return getRandomUnoCard();
+        }
+        else if (randomDouble <= 0.78) {
+            return getRandomMissingRoundCard();
+        }
+        else if (randomDouble <= 0.85) {
+            return getRandomChangingDirectionCard();
+        }
+        else if (randomDouble <= 0.92) {
+            return getRandomAddingCard();
+        }
+        else if (randomDouble <= 0.96) {
+            return getRandomWildcard();
+        }
+        else {
+            return getRandomAddingWildcard();
+        }
     }
 
     public Color getColor() {
@@ -37,19 +54,57 @@ public class UnoBoard extends AbstractBoard {
         this.clockwise = clockwise;
     }
 
-//    public Deque<UnoCard> getStackOfCard() {
-//        return stackOfCard;
-//    }
-//
-//    public void setStackOfCard(Deque<UnoCard> stackOfCard) {
-//        this.stackOfCard = stackOfCard;
-//    }
-
     public UnoCard getLastUsedCard() {
         return lastUsedCard;
     }
 
     public void setLastUsedCard(UnoCard lastUsedCard) {
         this.lastUsedCard = lastUsedCard;
+    }
+
+    private UnoCard getRandomUnoCard() {
+        return new UnoCard(getRandomColor(), getRandomInt());
+    }
+
+    private AddingCard getRandomAddingCard() {
+        return new AddingCard(2, getRandomColor());
+    }
+
+    private ChangingDirectionCard getRandomChangingDirectionCard() {
+        return new ChangingDirectionCard(getRandomColor());
+    }
+
+    private Wildcard getRandomWildcard() {
+        return new Wildcard();
+    }
+
+    private AddingWildcard getRandomAddingWildcard() {
+        return new AddingWildcard();
+    }
+
+    private MissingRoundCard getRandomMissingRoundCard() {
+        return new MissingRoundCard(getRandomColor());
+    }
+
+    private Color getRandomColor() {
+        Random random = new Random();
+        double randomColor = random.nextDouble();
+        if (randomColor <= 0.25) {
+            return Color.RED;
+        }
+        else if (randomColor <= 0.5) {
+            return Color.BLUE;
+        }
+        else if (randomColor <= 0.75) {
+            return Color.GREEN;
+        }
+        else {
+            return Color.YELLOW;
+        }
+    }
+
+    private int getRandomInt() {
+        Random random = new Random();
+        return random.nextInt(10);
     }
 }

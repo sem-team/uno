ecc7329525ac9ad4ca54672901edb9e988e06c4f
atheostheos <coal.marcos@gmail.com>
2021-12-27 11:36:00 +0300

    Added new message type

--- protocol/src/main/java/ru/kpfu/itis/sem_team/protocol/UnoProtocol.java
+++ protocol/src/main/java/ru/kpfu/itis/sem_team/protocol/UnoProtocol.java
@@ -5,6 +5,7 @@ public class UnoProtocol {
     public static final int MESSAGE_ROOM = 2;
     public static final int MESSAGE_GAME = 3;
     public static final int MESSAGE_CARD = 4;
+    public static final int MESSAGE_APP = 5;
 
     public static int STANDARD_PORT = 27027;
 }
